5fefa6b2a8141e2a16d5f440d1349b70cdb7e4e3
gangavarapukarthik <gkchowdary689@gmail.com>
2025-02-19 10:23:15 +0530

    index

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const http = require("http")
-const server = http.createServer((res,req)=>{
-    res.write("edho oka roju meru collector avuthaaru babu avutharu.....")
-    res.end("evvadu thaggatle.evad acting vaadu chesthunnadu")
+const http = require("http");
 
+const server = http.createServer((req, res) => {  // Ensure correct parameter order: req, res
+    // Set response header to indicate you're sending plain text
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    res.write("edho oka roju meru collector avuthaaru babu avutharu.....");
+    res.end("evvadu thaggatle.evad acting vaadu chesthunnadu");
 });
-server.listen("3001",()=>{
-    console.log("namasta sir")
-})
+
+server.listen(3100, () => {  // Ensure the server listens on the correct port
+    console.log("namasta sir");
+});
